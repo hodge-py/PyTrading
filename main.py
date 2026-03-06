2ddf78ec68dd0da88ce0d8a9a9cf21308a5d677b
Karson Hodge <105604814+hodge-py@users.noreply.github.com>
2026-03-05 22:50:54 -0700

    c

--- main.py
+++ main.py
@@ -2,8 +2,43 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import yfinance as yf
+import pathlib
 
+my_ticker = pd.read_csv('my_ticker.txt', header=None, skipinitialspace=True).iloc[0, :].tolist()
+
+print(my_ticker)
 # Sample data
+
+for x in my_ticker:
+    if not pathlib.Path(f'./pages/{x}.py').exists():
+        pathlib.Path(f'./pages/{x}.py').touch()
+        file_path = pathlib.Path(f'./pages/{x}.py')
+        python_code = f"""
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+def get_data(ticker):
+    data = yf.Ticker(ticker)
+    hist = data.history(period="1y")
+    return hist
+
+st.title("Stock Price Visualization")
+
+ticker = st.text_input("Enter a stock ticker symbol:", "{x}")
+if ticker:
+    data = get_data(ticker)
+    st.line_chart(data['Close'])
+
+            """
+        file_path.write_text(python_code)
+
+df_ticker = pd.read_csv('all_tickers.txt', sep='\t', header=0)
+
+print(df_ticker)
+
 df = yf.Ticker('GOOGL').history(period='1y')
 
 # 1. Native Streamlit Line Chart
---
+++ pages/MSFT.py
@@ -0,0 +1,20 @@
+
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+def get_data(ticker):
+    data = yf.Ticker(ticker)
+    hist = data.history(period="1y")
+    return hist
+
+st.title("Stock Price Visualization")
+
+ticker = st.text_input("Enter a stock ticker symbol:", "MSFT")
+if ticker:
+    data = get_data(ticker)
+    st.line_chart(data['Close'])
+
+            
